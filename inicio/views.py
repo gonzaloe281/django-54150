f53bdab26178ca26af64e92bc8a0cbb12fc5aab6
gonzalo <gonzaloe281@gmail.com>
2024-05-30 21:05:39 -0300

    clase 19 playgraund intermedio

--- inicio/views.py
+++ inicio/views.py
@@ -4,10 +4,14 @@ from datetime import datetime
 from django.http import HttpResponse
 from django.template import Template, Context, loader
 
+from inicio.models import Auto
+
 import random
 
 def inicio(request):
-    return HttpResponse('Bienvenidos a mi INICIO!!')
+# v1
+    # return HttpResponse('Bienvenidos a mi INICIO!!')
+    return render(request, 'inicio/index.html')
 
 def template1(request, nombre, apellido, edad):
     fecha = datetime.now()
@@ -72,4 +76,9 @@ def probando(request):
     
     numeros = random.choices(lista, k=50)
     
-    return render(request, 'probando_if_for', {'numeros': numeros})
+    return render(request, 'probando_if_for', {"numeros": numeros})
+
+def crear_auto(request, marca, modelo):
+    auto = Auto(marca = marca, modelo = modelo)
+    auto.save()
+    return render(request, 'auto_templates/creacion.html', {"auto": auto})
